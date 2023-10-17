068ff094766b2ba53c4a5830f48c549285eabc41
Nathan Barros <nathansousa0809@gmail.com>
2023-10-17 15:30:02 -0300

    Implemnetando a funcionalidade de login.

--- src/screens/login/index.js
+++ src/screens/login/index.js
@@ -1,24 +1,44 @@
-import React from "react";
-import { ScrollView, SafeAreaView, Text, TextInput, TouchableOpacity, View, Image } from "react-native";
+import React, { useState } from "react";
+import { ScrollView, SafeAreaView, Text, TextInput, TouchableOpacity, View, Image, Alert } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
+import login from '../../services/api/login'
 
 import estilos from "./estilo";
 
-function TelaLogin( { navigation } ){
-    return(
+function TelaLogin({ navigation }) {
+
+    // Variáveis de login
+    const [email, setEmail] = useState('renato@email.com');
+    const [senha, setSenha] = useState('renatin123');
+
+
+    return (
         <SafeAreaView style={estilos.tela}>
             <LinearGradient style={estilos.tela} colors={['#FFBFB1', '#FFFFFF']}>
                 <ScrollView style={estilos.tela}>
                     <View style={estilos.appbarLogin}>
-                        <Image style={estilos.navbarLogo} source={require("../../../assets/logo/navbarLogo.png")}/>
+                        <Image style={estilos.navbarLogo} source={require("../../../assets/logo/navbarLogo.png")} />
                     </View>
 
                     <View style={estilos.container}>
                         <Text style={estilos.titulo}>Login</Text>
 
-                        <TextInput placeholder="E-mail" style={estilos.input} />
-                        <TextInput placeholder="Senha" style={estilos.input} />
-                        <TouchableOpacity style={estilos.botao} onPress={() => { navigation.navigate( "Home" ) } }><Text style={estilos.txtBotao}>Entrar</Text></TouchableOpacity>
+                        <TextInput placeholder="E-mail" style={estilos.input} onChangeText={(texto) => setEmail(texto)} />
+                        <TextInput placeholder="Senha" style={estilos.input} onChangeText={(texto) => setSenha(texto)} />
+                        <TouchableOpacity style={estilos.botao} onPress={async () => {
+
+                            const usuarioLogado = await login(email, senha);
+
+                            if (usuarioLogado.status === 'success'){
+                                Alert.alert('Sucesso!', usuarioLogado.msg );
+
+                                navigation.navigate("Home");                                
+                            } else {
+                                Alert.alert('Ops... Algo deu errado.', usuarioLogado.msg );
+                            }
+
+
+                        }}><Text style={estilos.txtBotao}>Entrar</Text></TouchableOpacity>
 
                         <TouchableOpacity style={estilos.link}><Text style={estilos.txtLink}>Esqueci minha senha</Text></TouchableOpacity>
 
@@ -28,7 +48,7 @@ function TelaLogin( { navigation } ){
                             <View style={estilos.linha}></View>
                         </View>
 
-                        <TouchableOpacity style={estilos.link} onPress={() => { navigation.navigate ( "Cadastro" ) } }><Text style={estilos.txtLink}>Criar nova conta</Text></TouchableOpacity>
+                        <TouchableOpacity style={estilos.link} onPress={() => { navigation.navigate("Cadastro") }}><Text style={estilos.txtLink}>Criar nova conta</Text></TouchableOpacity>
                     </View>
                 </ScrollView>
             </LinearGradient>
---
+++ src/services/api/login.js
@@ -0,0 +1,30 @@
+import { url } from "../router/rota";
+
+export default async function login(email, senha){
+
+    const usuario = {
+        email: email,
+        senha: senha
+    }
+
+    try {
+
+        const requisicao = await fetch(`${url}/usuarios/login`, {
+            method: 'POST',
+            headers:{
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(usuario)
+        });
+
+        const resposta = await requisicao.json();
+
+        console.log(resposta);
+
+        return resposta;
+        
+    } catch (error) {
+        console.log(error);
+    }
+
+}
