c90bc49497e4fe8b0107c27bb56bdd9f06133e08
Nathan Barros <nathansousa0809@gmail.com>
2023-10-19 14:14:28 -0300

    Configurando as rotas e endpoints para API.

--- src/services/router/rota.js
+++ src/services/router/rota.js
@@ -1,2 +1,11 @@
 // Rota principal para API
 export const url = 'https://codigourmet-api.vercel.app'
+
+// Endpoints da API
+export const endpointUsuarios = '/usuarios';
+export const endpointLogin = '/login';
+export const endpointReceitas = '/receitas';
+export const endpointSenha = '/recuperarsenha';
+export const endpointMinhasReceitas = '/minhasreceitas';
+export const endpointReceitasfavoritas = '/receitasfavoritas';
+export const endpointApagarTudo = '/apagartudo';
