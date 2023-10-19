7e0e6f7e9d126b37e7df4d80ee0b5192dcbccd26
Nathan Barros <nathansousa0809@gmail.com>
2023-10-19 16:25:50 -0300

    Função para recuperação de senha.

---
+++ src/services/api/recuperarSenha/recuperarSenha.js
@@ -0,0 +1,50 @@
+import { url, endpointSenha } from "../../router/rota";
+
+// Função que manda o email para o usuário
+export async function enviarEmail(email) {
+
+    try {
+
+        const requisicao = await fetch(`${url}${endpointSenha}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({email})
+        });
+
+        const resposta = await requisicao.json();
+
+        return resposta;
+        
+    } catch (error) {
+        console.log(error);
+        const resposta = {msg: `Algo deu errado. Tente novamente mais tarde.`, status: `error`}
+        return resposta;
+    }
+
+}
+
+export async function recuperarSenha (id, senha) {
+
+    try {
+
+        const requisicao = await fetch(`${url}${endpointSenha}/${id}`, {
+            method: "PATCH",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({senha})
+        });
+
+        const resposta = await requisicao.json();
+
+        return resposta;
+        
+    } catch (error) {
+        console.log(error);
+        const resposta = {msg: `Algo deu errado. Tente novamente mais tarde.`, status: `error`}
+        return resposta;
+    }
+
+}
