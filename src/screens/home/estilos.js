9841b59ada2e38d3a88ef15e8b8c4cfe2989b2d9
Renato <renatoviturino80@gmail.com>
2023-10-24 15:24:12 -0300

    Estilizando a tela home e alguns detalhes do drawer

---
+++ src/screens/home/estilos.js
@@ -0,0 +1,53 @@
+import { StyleSheet } from "react-native";
+
+export const estilos = StyleSheet.create({
+    tela:{
+        height: "100%",
+    },
+
+    cor:{
+        backgroundColor: "#F8836A",
+    },
+
+    container:{
+        width: "80%",
+        height: 40,
+        
+        flexDirection: "row",
+        alignSelf: "center",
+        
+        marginTop: 100,
+    },
+
+    busca:{
+        width: "80%",
+        
+        backgroundColor: "#FFF",
+
+        borderTopLeftRadius: 20,
+        borderBottomLeftRadius: 20,
+        
+        borderWidth: 2,
+        borderColor: "#FF2D00",
+
+        padding: 10,
+    },
+
+    btnBusca:{
+        backgroundColor: "#F9441E",
+        width: "20%",
+        borderTopRightRadius: 20,
+        borderBottomRightRadius: 20,
+
+        alignItems: "center",
+        justifyContent: "center",
+
+    },
+
+    imgBusca:{
+        width: 30,
+        height: 30,
+
+        marginRight: 5,
+    }
+})
--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,25 +1,32 @@
 import React, { useState } from "react";
-import { ScrollView, Text } from "react-native";
+import { ScrollView, Text, SafeAreaView, TextInput, View, TouchableOpacity, Image } from "react-native";
+import { estilos } from "./estilos";
+
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 function TelaHome(){
-    // Criando uma variável para armazenar o objeto/array que virá no Async Storage
-    const [usuario, setUsuario] = useState(''); 
+    // // Criando uma variável para armazenar o objeto/array que virá no Async Storage
+    // const [usuario, setUsuario] = useState(''); 
 
-    // Função para coletar o objeto/array no Async Storage
-    async function getData(){
-        const resposta = await AsyncStorage.getItem('dados');
-        const objeto = resposta != null ? JSON.parse(resposta) : null;
+    // // Função para coletar o objeto/array no Async Storage
+    // async function getData(){
+    //     const resposta = await AsyncStorage.getItem('dados');
+    //     const objeto = resposta != null ? JSON.parse(resposta) : null;
 
-        setUsuario(objeto);
-    };
+    //     setUsuario(objeto);
+    // };
 
-    getData();
+    // getData();
 
     return(
-        <ScrollView>
-            <Text>{ JSON.stringify(usuario) }</Text>
-        </ScrollView>
+        <SafeAreaView style={ [estilos.tela, estilos.cor] }>
+            <ScrollView style={ estilos.cor }>
+                <View style={ estilos.container }>
+                    <TextInput placeholder="Encontre uma receita" style={ estilos.busca } />
+                    <TouchableOpacity style={estilos.btnBusca}><Image style={estilos.imgBusca} source={require('../../../assets/icons/search.png')} /></TouchableOpacity>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 
