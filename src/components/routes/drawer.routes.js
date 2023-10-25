2c34b49cdf0f748557e1309c276f396e4bf38fcb
Nathan Barros <nathansousa0809@gmail.com>
2023-10-25 15:30:49 -0300

    Consertando a tela de perfil.

--- src/components/routes/drawer.routes.js
+++ src/components/routes/drawer.routes.js
@@ -18,16 +18,16 @@ function DrawerRoute(){
     return(
         <NavigationContainer>
             <Drawer.Navigator initialRouteName="Inicial" screenOptions={ {title: "", drawerStyle: { backgroundColor: "#F85836" }, drawerItemStyle: { borderBottomWidth: 2, borderColor: "#F8836A" }, headerTransparent: true, drawerActiveBackgroundColor: "#F8836A", drawerLabelStyle: { color: "#FFFFFF", fontWeight: "bold" } } }>
-                <Drawer.Screen name="Home" component={TelaHome} />
-                <Drawer.Screen name="Perfil" component={TelaPerfil} />
+                <Drawer.Screen name="Home" component={TelaHome} options={ { drawerLabel: "Home" } }/>
+                <Drawer.Screen name="Perfil" component={TelaPerfil} options={ { drawerLabel: "Meu Perfil" } } />
                 <Drawer.Screen name="Receita" component={TelaCriarReceita}/> 
                 <Drawer.Screen name="Cadastro" component={TelaCadastro} options={ { headerShown: false, drawerItemStyle: { display: "none" } } } />
                 <Drawer.Screen name="Login" component={TelaLogin} options={ { headerShown: false, drawerLabel: "Sair" } } />
                 <Drawer.Screen name="Inicial" component={TelaInicial} options={{ headerShown: false, drawerItemStyle: {display: "none"}}}/>
                 <Drawer.Screen name="EnviarEmail" component={TelaEnviarEmail} options={{ headerShown: false, drawerItemStyle: {display: "none"}}}/>
                 <Drawer.Screen name="RecuperarSenha" component={TelaRecuperarSenha} options={{ headerShown: false, drawerItemStyle: {display: "none"}}}/>
-                <Drawer.Screen name="CriarReceita" component={TelaCriarReceita} options={{ headerShown: false, drawerItemStyle: {display: "none"}}}/>
-                <Drawer.Screen name="MinhasReceitas" component={TelaMinhasReceitas}/> 
+                <Drawer.Screen name="CriarReceita" component={TelaCriarReceita} options={{ headerShown: false, drawerItemStyle: {display: "none"}, drawerLabel: "Criar Receita" }}/>
+                <Drawer.Screen name="MinhasReceitas" component={TelaMinhasReceitas} options={{ drawerLabel: "Minhas Receitas" }} /> 
             </Drawer.Navigator>
         </NavigationContainer>
     )
--- src/screens/perfil/index.js
+++ src/screens/perfil/index.js
@@ -1,87 +1,82 @@
 import React, { useState, useEffect } from "react";
 import { ScrollView, Text, SafeAreaView, View, TextInput, TouchableOpacity, Image, Alert } from "react-native";
 import { estilos } from "./estilos";
-// import excluirUsuario from "../../services/api/usuario/excluirUsuario";
-
-//  RESOLVER ISSO AQUI
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-function TelaPerfil( { navigation } ){
+function TelaPerfil({ navigation }) {
+  const [dados, setDados] = useState(null);
 
-    const [dados, setDados] = useState('');
+  useEffect(() => {
+    async function getData() {
+      const resposta = await AsyncStorage.getItem('dados');
+      const objeto = resposta != null ? JSON.parse(resposta) : null;
 
-    
-    useEffect(() => {
-        async function getData(){
-            const resposta = await AsyncStorage.getItem('dados');
-            const objeto = resposta != null ? JSON.parse(resposta) : null;
+      setDados(objeto);
+    }
 
-    
-           setDados(objeto);
-        }
+    getData();
+  }, []);
 
-        getData();
-    }, [])
+  return (
+    <SafeAreaView style={estilos.tela}>
+      <ScrollView style={estilos.corDeFundo}>
+        <View style={[estilos.centralizar, estilos.container]}>
+          <Image style={estilos.imagem} source={require("../../../assets/icons/chef_icon.png")} />
+          <Text style={estilos.titulo}>Meu perfil</Text>
+        </View>
 
-    console.log(dados);
+        <View style={[estilos.centralizar, estilos.formulario]}>
+          {dados && dados[0] && (
+            <View style={estilos.containerInput}>
+              <Text style={estilos.label}>Nome</Text>
+              <TextInput style={estilos.input} value={dados[0].nome} editable={false} />
+            </View>
+          )}
 
-    // const nome = dados[0]['nome'];
-    // const sobrenome = dados[0]['sobrenome'];
-    // const email = dados[0]['email'];
+          {dados && dados[0] && (
+            <View style={estilos.containerInput}>
+              <Text style={estilos.label}>Sobrenome</Text>
+              <TextInput style={estilos.input} value={dados[0].sobrenome} editable={false} />
+            </View>
+          )}
 
-    // const nome = 'oi';
-    // const sobrenome = 'olá';
-    // const email = 'oi@oi.com';
+          {dados && dados[0] && (
+            <View style={estilos.containerInput}>
+              <Text style={estilos.label}>E-mail</Text>
+              <TextInput style={estilos.input} value={dados[0].email} editable={false} />
+            </View>
+          )}
 
-    return(
-        <SafeAreaView style={estilos.tela}>
-            <ScrollView style={estilos.corDeFundo}>
-                <View style={[estilos.centralizar, estilos.container]}>
-                    <Image style={estilos.imagem} source={require("../../../assets/icons/chef_icon.png")}/>
-                    <Text style={estilos.titulo}>Meu perfil</Text>
-                </View>
+          <TouchableOpacity style={estilos.botao}>
+            <Text style={estilos.txtBotao}>Alterar senha</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={estilos.botao} onPress={() => {
+            Alert.alert('Cuidado!', 'Você está prestes a excluir sua conta. Essa ação não pode ser revertida!', [
+              {
+                text: "Cancelar"
+              },
 
-                <View style={[estilos.centralizar, estilos.formulario]}>
-                    <View style={estilos.containerInput}>
-                        <Text style={estilos.label}>Nome</Text>
-                        <TextInput style={estilos.input} editable={false} />
-                    </View>
+              {
+                text: "Estou ciente!",
+                onPress: () => {
+                  excluirUsuario(dados[0]['id'], dados[1])
+                },
+                style: "cancel"
+              }
+            ])
+          }}>
+            <Text style={estilos.txtBotao}>Excluir conta</Text>
+          </TouchableOpacity>
 
-                    <View style={estilos.containerInput}>
-                        <Text style={estilos.label}>Sobrenome</Text>
-                        <TextInput style={estilos.input}  editable={false} />
-                    </View>
-
-                    <View style={estilos.containerInput}>
-                        <Text style={estilos.label}>E-mail</Text>
-                        <TextInput style={estilos.input} editable={false} />
-                    </View>
-
-                    <TouchableOpacity style={estilos.botao}><Text style={estilos.txtBotao}>Alterar senha</Text></TouchableOpacity>
-                    <TouchableOpacity style={estilos.botao} onPress={() => {
-                        Alert.alert('Cuidado!', 'Você está prestes a excluir sua conta. Essa ação não pode ser revertida!', [
-                            {
-                                text: "Cancelar"
-                            },
-                            
-                            {
-                                text: "Estou ciente!",
-                                onPress: () => {
-                                    excluirUsuario(dados[0]['id'], dados[1])
-                                },
-                                style: "cancel"
-                            }
-                        ])
-                    }}><Text style={estilos.txtBotao}>Excluir conta</Text></TouchableOpacity>
-
-                    <TouchableOpacity onPress={() => {
-                        navigation.navigate("Home")
-                    }}><Text style={estilos.link}>Voltar</Text></TouchableOpacity>
-
-                </View>
-            </ScrollView>
-        </SafeAreaView>
-    )
+          <TouchableOpacity onPress={() => {
+            navigation.navigate("Home")
+          }}>
+            <Text style={estilos.link}>Voltar</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  )
 }
 
 export default TelaPerfil;
